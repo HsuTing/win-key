80c800532a182e6da9c1c0bbc897642bab5de115
Emma <emma2334@gmail.com>
2024-10-08 16:29:11 +0800

    style: reset container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+      },
+    },
     extend: {
       colors: {
         pm: {
