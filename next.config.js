b87ef66e48f041de70efdb6fe05d37053bb678ea
Emma <emma2334@gmail.com>
2024-11-07 19:20:47 +0800

    chore: add gh-pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const { i18n } = require('./next-i18next.config')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  i18n,
+  // i18n,
+  output: 'export',
+  assetPrefix: isProd ? '/win-key/' : '',
+  basePath: isProd ? '/win-key' : '',
 }
