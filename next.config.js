4ed2f22abc2bd33a5ca4ec8687cdf9ba404186e6
Emma <emma2334@gmail.com>
2024-11-08 15:16:27 +0800

    feat: add CNAME while building

--- next.config.js
+++ next.config.js
@@ -5,6 +5,4 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   // i18n,
   output: 'export',
-  assetPrefix: isProd ? '/win-key/' : '',
-  basePath: isProd ? '/win-key' : '',
 }
