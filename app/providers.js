4f5c89dd399d7bdc22c0646cbffd58d92154ffd6
HsuTing <hsuting0106@gmail.com>
2025-06-28 15:10:45 +0800

    fix(app): fix i18n

--- app/providers.js
+++ app/providers.js
@@ -3,8 +3,8 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next, I18nextProvider } from "react-i18next";
-import { useEffect } from "react";
 import locales from "@/locales";
+import { useParams } from "next/navigation";
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
   resources: locales,
@@ -12,6 +12,16 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
 });
 
 export function Providers({ children }) {
+  const params = useParams();
+
+  if (typeof window === "undefined" && params?.lang) {
+    i18n.init({
+      lng: params.lang,
+      resources: locales,
+      fallbackLng: "en",
+    });
+  }
+
   return (
     <ParallaxProvider>
       <I18nextProvider i18n={i18n} defaultNS={"translation"}>
