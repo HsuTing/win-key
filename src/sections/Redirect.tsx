1f52ca27907ad9b654d5b510f9670643b418a554
HsuTing <hsuting0106@gmail.com>
2025-07-04 21:41:52 +0800

    fix(redirect): fix default page

--- src/sections/Redirect.tsx
+++ src/sections/Redirect.tsx
@@ -6,9 +6,11 @@ import i18n from "i18next";
 const Redirect = () => {
   const router = useRouter();
   const LANGUAGE = ["en", "zh-TW", "ja"] as const;
+
   useEffect(() => {
-    router.replace(`/${i18n.language in LANGUAGE ? i18n.language : "en"}`);
+    router.replace(`/${LANGUAGE.includes(i18n.language as typeof LANGUAGE[number]) ? i18n.language : "en"}`);
   });
+
   return <></>;
 };
 
